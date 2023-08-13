90df6873717c1d936a1a45c26fb2dfc4ac281088
rsnf <roberto.sarquis@gmail.com>
2023-08-13 07:48:13 -0300

    Inital import.

---
+++ spring-mvc-security/src/main/java/com/sqs/springdemo/mvc/security/DemoSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.sqs.springdemo.mvc.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsManager() {
+
+	// @formatter:off
+	UserDetails john = User.builder()
+		.username("john")
+		.password("{noop}test123")
+		.roles("EMPLOYEE")
+		.build();
+	
+	UserDetails mary = User.builder()
+		.username("mary")
+		.password("{noop}test123")
+		.roles("EMPLOYEE", "MANAGER")
+		.build();
+	
+	UserDetails susan = User.builder()
+		.username("susan")
+		.password("{noop}test123")
+		.roles("EMPLOYEE", "MANAGER", "ADMIN")
+		.build();
+	// @formatter:on
+
+	return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+	// @formatter:off
+	http.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
+		.formLogin(form -> form
+			.loginPage("/showMyLoginPage")
+			.loginProcessingUrl("/authenticateTheUser")
+			.permitAll());
+	// @formatter:on
+
+	return http.build();
+    }
+}
