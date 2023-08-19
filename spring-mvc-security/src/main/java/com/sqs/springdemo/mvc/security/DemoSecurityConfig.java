ac0f95b25c40bb35fc5204351b8596e8270bcae6
rsnf <roberto.sarquis@gmail.com>
2023-08-19 15:28:06 -0300

    Spring MVC Security - JDCB Authentication - Custom Tables with BCrypt Encryptation use: sql-scripts: 06

--- spring-mvc-security/src/main/java/com/sqs/springdemo/mvc/security/DemoSecurityConfig.java
+++ spring-mvc-security/src/main/java/com/sqs/springdemo/mvc/security/DemoSecurityConfig.java
@@ -15,7 +15,13 @@ public class DemoSecurityConfig {
     @Bean
     UserDetailsManager userDetailsManager(DataSource dataSource) {
 
-	return new JdbcUserDetailsManager(dataSource);
+	JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+	jdbcUserDetailsManager.setUsersByUsernameQuery("SELECT user_id, pw, active FROM members WHERE user_id=?");
+
+	jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT user_id, role FROM roles WHERE user_id=?");
+
+	return jdbcUserDetailsManager;
     }
 
     @Bean
